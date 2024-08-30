89c467206935dd66d548ee0984606392dc43fe4f
Moschifire <markoladeinde@gmail.com>
2024-08-30 17:54:10 +0100

    modifications to problem type selector

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ProblemTypeSelector from "./ProblemTypeSelector";
+import { ProblemInput } from "./ProblemInput";
 
 function App() {
+  const [problemType, setProblemType] = useState("Basic Operations");
+  const [input, setInput] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Maths Solver</h1>
+      <ProblemTypeSelector onSelectType={setProblemType} />
+      <ProblemInput problemType={problemType} onSolve={setInput} />
     </div>
   );
 }
---
+++ src/ProblemTypeSelector.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ProblemTypeSelector = ({ onSelectType }) => {
+  const problemType = [
+    "Basic Operations",
+    "Simultaneous Equations",
+    "Quadratic Equations",
+    "Surds",
+    "Calculus",
+  ];
+  return (
+    <select onChange={(e) => onSelectType(e.target.value)}>
+      {problemType.map((type) => (
+        <option key={type} value={type}>
+          {type}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default ProblemTypeSelector;
